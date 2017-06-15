fff23ac3e7b6de3354c9022553a7317415eda028
João Filipe Vaz <joaofilipevaz@gmail.com>
2017-06-15 01:08:15 +0100

    wip

--- trab_4.py
+++ trab_4.py
@@ -8,6 +8,8 @@
 import cv2
 import numpy as np
 from time import time
+import matplotlib.pyplot as plt
+import Queue
 from os import path
 
 
@@ -19,14 +21,41 @@ cador já disponível
 """
 
 
-def comprime():
+def intra_frame_coding():
+    print "Analise INTRA-FRAME"
+    print "========================================================================================================"
 
     for i in xrange(1, 12, 1):
 
-        cv2.imwrite("output/bola_{}.jpeg".format(i), cv2.imread("samples/bola_{}.tiff".format(i)),
-                    (cv2.IMWRITE_JPEG_QUALITY, 50))
-        cv2.imwrite("output/car{}.jpeg".format(i), cv2.imread("samples/car{}.bmp".format(i)),
-                    (cv2.IMWRITE_JPEG_QUALITY, 50))
+        # leitura imagem bola
+        x_bola = cv2.imread("samples/bola_{}.tiff".format(i))
+
+        # leitura imagem car
+        # x_car = cv2.imread("samples/car{}.bmp".format(i))
+
+        # conversão e escrita com factor de qualidade 50
+        cv2.imwrite("output/bola_{}.jpeg".format(i), x_bola, (cv2.IMWRITE_JPEG_QUALITY, 50))
+
+        x_bola_desc = cv2.imread("output/bola_{}.jpeg".format(i))
+
+        # conversão e escrita com factor de qualidade 50
+        # x_car_desc = cv2.imwrite("output/car{}.jpeg".format(i), x_car, (cv2.IMWRITE_JPEG_QUALITY, 50))
+
+        print "ANALISE FRAME " + str(i)
+        # calculo SNR bola
+        print "SNR = " + str(calculoSNR(x_bola, x_bola_desc))
+
+        size_ini = path.getsize("samples/bola_{}.tiff".format(i))
+        size_end = path.getsize("output/bola_{}.jpeg".format(i))
+        print "A dimensão da frame original é de {} Kb".format(round(size_ini / 1024., 2))
+        print "A dimensão da frame codificada é de {} Kb".format(round(size_end / 1024., 2))
+        print "A taxa de compressão conseguida foi de {}".format(1. * size_ini / size_end)
+
+        # Calcula o histogram
+        h, bins, patches = plt.hist(x_bola_desc.ravel(), np.max(x_bola_desc), [0, np.max(x_bola_desc)])
+
+        entropia(x_bola_desc, gera_huffman(h))
+        print "========================================================================================================"
 
 
 def cod_intraframe():
@@ -97,64 +126,91 @@ def calculoSNR(imgOrig, imgComp):
     return np.round(10.0*np.log10(args), 3)
 
 
+def gera_huffman(freq):
+    # simbolos do algoritmo - neste caso uma gama de valores numa lista de listas
+    simbolos = [[i] for i in xrange(len(freq))]
+
+    # String vazia para guardar os bits resultantes da codificacao
+    bits = ["" for y in xrange(len(freq))]
+
+    # Cria um array de arrays (numero de ocorrencias,valor, codificação) organizado por ordem crescente
+    tabela = sorted(list(t) for t in zip(freq, simbolos, bits))
+
+    # remove ocorrencias nulas do array
+    while tabela[0][0] == 0:
+        tabela.remove(tabela[0])
+
+    # implementa uma fila para simular o funcionamento do algoritmo
+    p = Queue.PriorityQueue()
+
+    # popula a fila com os valores do array
+    for value in tabela:
+        p.put(value)
+
+    # enquanto o fila tiver mais que um nó
+    while p.qsize() > 1:
+
+        # extrai os dois nós esquerda e direita com o valor mais pequeno
+        l, r = p.get(), p.get()
+
+        # itera no array de valores e nos simbolos dos nós
+        for i in xrange(len(tabela)):
+            for z in xrange(len(l[1])):
+
+                # se o simbolo no array for igual ao simbolo do nó extraido
+                if tabela[i][1][0] == l[1][z]:
+
+                    # para o no da esquerda guarda o bit 0 na codificação
+                    tabela[i][2] += '0'
+            for t in xrange(len(r[1])):
+                if tabela[i][1][0] == r[1][t]:
+                    tabela[i][2] += '1'
+        # adiciona a soma dos nós a queue
+        p.put([l[0] + r[0], l[1] + r[1]])
+
+    # inverte a string com a codificação para representar o percurso da raiz até as folhas
+    for i in xrange(len(tabela)):
+        tabela[i][2] = tabela[i][2][::-1]
+
+    print "=========================================Tabela Codigo=================================================="
+    # imprime a array resultante em formato tabela
+    print "Peso\tSimbolo\tCodificação"
+    for t in range(len(tabela)):
+        print "%-6s\t%-4s\t%-4s" % (tabela[t][0], tabela[t][1][0], tabela[t][2])
+    print "========================================================================================================"
+
+    return tabela
+
+
+def entropia(x, tabela_codigo):
+
+    # entropia
+    hx = 0
+
+    # numero médio de bits por simbolo
+    l = 0
+
+    for i in xrange(len(tabela_codigo)):
+        prob = tabela_codigo[i][0] / len(x)
+        hx += prob * np.log2(1 / prob)
+        l += prob * len(tabela_codigo[i][2])
+
+    # Eficiencia do codigo
+    efic = hx / l
+
+    print "A Entropia é de {} bits/símbolo".format(hx)
+    print "O numero médio de bits por símbolo é de {}".format(l)
+    print "A eficiência do código é {}".format(efic)
+
+
 """
 Main
 """
 
 def main():
     print "========================================================================================================"
-    print "================================Analise Ficheiro lena================================" \
-          "======="
 
-
-
-    t1 = time()
-    print "O tempo necessário para efectuar a DCT e a Quantificação foi de {} segundos".format(round(t1 - t0, 3))
-
-
-
-    t2 = time()
-    print "O tempo necessário para efectuar a codificação DC foi de {} segundos".format(round(t2 - t1, 3))
-
-
-
-    t3 = time()
-    print "O tempo necessário para efectuar a codificação AC foi de {} segundos".format(round(t3 - t2, 3))
-
-
-
-    t4 = time()
-    print "O tempo necessário para o bloco de entropy coding (huffman) foi de {} segundos".format(round(t4 - t3, 3))
-
-
-
-    t5 = time()
-    print "O tempo necessário para a leitura do ficheiro e reconstrução do ac e dc foi de {} " \
-          "segundos".format(round(t5 - t4, 3))
-
-
-    t6 = time()
-    print "O tempo necessário para a descodificacao ac foi de {} segundos".format(round(t6 - t5, 3))
-
-
-
-    t7 = time()
-    print "O tempo necessário para a descodificacao dc foi de {} segundos".format(round(t7 - t6, 3))
-
-
-    t8 = time()
-    print "O tempo necessário para a dct inversa dc foi de {} segundos".format(round(t8 - t7, 3))
-
-
-    print "factor q = " + str(q)
-    print "alfa = " + str(alfa)
-    print "SNR = " + str(calculoSNR(x, x_rec))
-    size_ini = path.getsize("samples/lena.tiff")
-    size_end = path.getsize("Lena descodificada Q = {}.jpeg".format(q))
-    print "A dimensão do ficheiro original é de {} Kb".format(round(size_ini / 1024., 2))
-    print "A dimensão do ficheiro codificado é de {} Kb".format(round(size_end / 1024., 2))
-    print "A taxa de compressão conseguida foi de {}".format(1. * size_ini / size_end)
-    print "O saldo da compressão foi de {} Kb".format(round((size_ini - size_end) / 1024., 2))
+    intra_frame_coding()
 
     print "========================================================================================================"
     print "========================================================================================================"
